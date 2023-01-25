166745e5315d9b2c557e09fc672b46e3884c8dcc
Princeton D'souza <princeton2102@gmail.com>
2023-01-25 20:30:59 +0530

    steps added

---
+++ src/pages/Home/Steps/StepsData.js
@@ -0,0 +1,27 @@
+[
+    {
+        id: 1,
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, iste!" 
+    },
+    {
+        id: 2,
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, iste!" 
+    },
+    {
+        id: 3,
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, iste!" 
+    },
+    {
+        id: 4,
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, iste!" 
+    },
+    {
+        id: 5,
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, iste!" 
+    },
+    {
+        id: 6,
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, iste!" 
+    }
+
+]
