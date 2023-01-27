9b249b34779c79adf8a6394cb225f8097ce68ab1
Sapna Kul <kulsapna2210002@gmail.com>
2023-01-28 01:21:11 +0530

    create trip form done

--- src/pages/Home/Plan_a_trip/PlanTrip.jsx
+++ src/pages/Home/Plan_a_trip/PlanTrip.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import "./PlanTrip.css";
 const PlanTrip = () => {
   return (
-    <div className="plan_bg">
+    <>
+     
+    <div className="plan">
       <div className="plan_heading">
-        <h1>Create Your Trip</h1>
+        <h1>Create Your <span>Trip</span></h1>
       </div>
+
+      <div className="downSec">
+      <div className="colOne">
       <div className="plan_form">
         <div className="form_item">
+          
           <div className="form_heading">
-            <h3>Trip Name</h3>
+            <h3>TripName</h3>
           </div>
           <div className="item_input">
             <input type="text" name="trip_name" id="trip_name" />
           </div>
+
         </div>
         <div className="form_item">
           <div className="form_heading">
@@ -24,24 +31,29 @@ const PlanTrip = () => {
           </div>
         </div>
         <div className="form_item_special">
-          <div className="form_heading">
+          <div className="dateD">
             <h3>From</h3>
+            <h3>To</h3>
           </div>
+          <div className="dates">
           <div className="item_input">
             <input type="date" name="" id="from_date" />
-          </div>
-          <div className="form_heading">
-            <h3>To</h3>
           </div>
           <div className="item_input">
             <input type="date" name="" id="to_date" />
           </div>
+          </div>
         </div>
-        <div className="plan_button">
-          <button type="submit">Create</button>
-        </div>
+        
+<button>
+    Create
+</button>
+      </div>
+      </div>
+      <div className="colTwo"></div>
       </div>
     </div>
+    </>
   );
 };
 
